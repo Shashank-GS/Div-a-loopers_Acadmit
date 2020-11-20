5e78575cd287438e7aecad815bb4ec05d60aa83f
tejasimangale <tejasimangale@gmail.com>
2020-11-20 19:54:43 +0530

    login and signup

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -17,6 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/studentDashboard/:username',
+    name:"StudentDashboard",
+    component: () => import('../views/Signup.vue'),
+    props:true
   }
 ]
 
