ce7c6592d92498f84a7e44e1541af6e92d38ce57
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2020-11-20 20:16:39 +0530

    added page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,11 +2,33 @@
   <div class="about">
     <Navbar></Navbar>
     <section>
-      <h1>Welcome Lorem</h1>
+      <h1>Welcome Lorem !</h1>
       <h2>Create an account to proceed further</h2>
       <button v-if="show == false" @click.prevent="showform">
         Create account
       </button>
+      <div class="content" v-if="show == false">
+        <h1>Important!</h1>
+        <ul>
+          <li>
+            This will require you to fill personal details such as name ,
+            education , religion etc.
+          </li>
+          <li>
+            This information will be used to fill the basic details of the
+            admission form of college, make sure the details entered are correct
+            and valid.
+          </li>
+          <li>
+            You can edit your details anytime after creating your account.
+          </li>
+          <li>
+            Your account details will be kept secure and will only be used for
+            only filling admission form Only you have the editing rights of your
+            account.
+          </li>
+        </ul>
+      </div>
       <form action="" v-if="show == true">
         <label for="fname">First name:</label><br />
         <input v-model="fname" type="text" /><br />
@@ -76,12 +98,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&family=Abril+Fatface&display=swap");
 section {
+  margin-left: 3%;
   min-height: 100vh; /* will cover the 100% of viewport */
   overflow: hidden;
   display: block;
   position: relative;
   padding-bottom: 212.109px;
+  margin-top: 2%;
 }
 .about {
   height: 100%;
@@ -94,5 +119,33 @@ select {
 form {
   margin-top: 30px;
   margin-left: 30px;
+}
+section h1 {
+  font-size: 2rem;
+  font-family: "Montserrat";
+  font-weight: bold;
+}
+section h2 {
+  font-family: "Montserrat";
+  font-weight: lighter;
+  font-size: 1.4rem;
+}
+button {
+  background-color: #000d3a;
+  color: white;
+  padding: 0.5%;
+  border-radius: 5px;
+  font-size: "poppins";
+}
+.content {
+  margin-top: 3%;
+}
+.content h1 {
+  font-size: 1.3rem;
+  font-weight: lighter;
+  margin-bottom: 2%;
+}
+.content ul li {
+  margin-bottom: 2%;
 }
 </style>
