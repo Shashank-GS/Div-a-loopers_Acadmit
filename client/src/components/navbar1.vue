acba7b3d184357e21dc42e06f9f03c1e898cb1df
tejasimangale <tejasimangale@gmail.com>
2020-11-20 10:10:48 +0530

    Routers and pages setup, Navbar added

---
+++ client/src/components/navbar1.vue
@@ -0,0 +1,111 @@
+<template>
+    <nav>
+        <div class="logo">ACADMIT</div>
+        <a href="#" class="toggle-button">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </a>
+        <div class="nav-links">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Institutes</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+*{
+    text-decoration: none;
+    list-style: none;
+}
+nav{
+    display: flex;
+    position: relative;
+    justify-content: space-between;
+    align-items: center;
+    background: #000D3A;
+    color: white;
+}
+.logo{
+    font-family: Roboto;
+    font-size: 1.5rem;
+    margin: 1rem;
+}
+.nav-links {
+    height: 100%;
+}
+
+.nav-links ul {
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+
+.nav-links li a {
+    display: block;
+    color: white;
+    padding: 1rem;
+}
+
+.nav-links li a:hover {
+    color:rgb(88, 160, 228);
+}
+
+.toggle-button {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 30px;
+    height: 21px;
+}
+
+.toggle-button .bar {
+    height: 3px;
+    width: 100%;
+    background-color: white;
+    border-radius: 10px;
+}
+
+@media (max-width: 800px) {
+    nav{
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .toggle-button {
+        display: flex;
+    }
+
+    .nav-links {
+        display: none;
+        width: 100%;
+    }
+
+    .nav-links ul {
+        width: 100%;
+        flex-direction: column;
+    }
+
+    .nav-links ul li {
+        text-align: center;
+    }
+
+    .nav-links ul li a {
+        padding: .5rem 1rem;
+    }
+
+    .nav-links.active {
+        display: flex;
+    }
+}
+
+
+
+</style>
